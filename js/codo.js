95c751d4ed9884c4a4da42e46ec9ba46dbec5226
luiscibau <72272905+luiscibau@users.noreply.github.com>
2023-05-15 00:56:30 -0300

    Add files via upload

--- js/codo.js
+++ js/codo.js
@@ -4,69 +4,79 @@ function mostrar_menu(){
 
     document.querySelector(".menu").classList.toggle("mostrar_menu");
 }
+/****************************** */
 
-function suma(a, b) {
-    return a + b;
-  }
+///data para el carousel ////
+
+/****************************** */
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    const carousel0 = document.querySelector(".carousel0");
+    const slides = Array.from(carousel0.children);
+    const prevButton = document.getElementById("prevButton");
+    const nextButton = document.getElementById("nextButton");
+    const slideWidth = slides[0].getBoundingClientRect().width;
+    let currentIndex = 0;
   
-let numero = 47;
-if (numero > 10) {
-    console.log("el $$$$$ numero es mayor que 10");
-} else {
-    console.log("el numero es menor o igual que 10 ");
-}
+    function moveToSlide(carousel0, currentSlide, targetSlide) {
+      carousel0.style.transform = `translateX(-${targetSlide.offsetLeft}px)`;
+      currentSlide.classList.remove("active");
+      targetSlide.classList.add("active");
+    }
+  
+    // Move to next slide
+    function nextSlide() {
+      const currentSlide = slides[currentIndex];
+      currentIndex = (currentIndex + 1) % slides.length;
+      const targetSlide = slides[currentIndex];
+      moveToSlide(carousel0, currentSlide, targetSlide);
+    }
+  
+    // Move to previous slide
+    function prevSlide() {
+      const currentSlide = slides[currentIndex];
+      currentIndex = (currentIndex - 1 + slides.length) % slides.length;
+      const targetSlide = slides[currentIndex];
+      moveToSlide(carousel0, currentSlide, targetSlide);
+    }
+  
+    nextButton.addEventListener("click", nextSlide);
+    prevButton.addEventListener("click", prevSlide);
+  
+    setInterval(nextSlide, 5000); // Cambiar de slide cada 5 segundos
+  });
 
-let dia= "Feriado";
-
-switch (dia) {
-    case "lunes":
-    console.log("hoy es lunes");
-    break;
-    case "Martes":
-    console.log("hoy es Martes");
-    break;
-    case "Miercoles":
-    console.log("hoy es Miercoles");
-    break;
-    case "Jueves":
-    console.log("hoy es Jueves");
-    break;
-    case "Viernes":
-    console.log("hoy es Viernes");
-    break;
-    case "Sabado":
-    console.log("hoy es Sabado");
-    break;
-    case "Domingo":
-    console.log("hoy es Domingo");
-    break;
-    default:
-    console.log("Ese Dia no Existe");
-    break;
-
-}
-
-let persona = {
-    nombre:"Juan",
-    edad: 35,
-    profesion: "Medico, Especialista",
-    saludar: function() {
-        console.log("Hola, agregando la funcion al saludo, mi nombre es " 
-        + this.nombre + " y mi profesion es " 
-        + this.profesion + " con una edad de " 
-        + this.edad);
-      }
-    };
-    
-persona.saludar();
+  /* para  productos a la venta*/
 
 
-console.log(persona.nombre);
-console.log(persona.edad);
-console.log(persona.profesion);
+  /*****************************/
+  // //////////Slider 1 /////////
 
-persona.nacionalidad = "Argentina";
+const carousel1 = document.querySelector('.productosEnVenta .carousel1');
+const prevButton1 = document.getElementById('prevButton1');
+const nextButton1 = document.getElementById('nextButton1');
 
-console.log(persona);
+let direction1 = -1;
+let currentPosition1 = 0;
 
+nextButton1.addEventListener('click', () => {
+  currentPosition1 += direction1 * 310;
+  if (currentPosition1 > 0) {
+    currentPosition1 = -310;
+  } else if (currentPosition1 < -310) {
+    currentPosition1 = 0;
+  }
+  carousel1.style.transform = `translateX(${currentPosition1}px)`;
+});
+
+prevButton1.addEventListener('click', () => {
+  currentPosition1 -= direction1 * 310;
+  if (currentPosition1 > 0) {
+    currentPosition1 = -310;
+  } else if (currentPosition1 < -310) {
+    currentPosition1 = 0;
+  }
+  carousel1.style.transform = `translateX(${currentPosition1}px)`;
+});
 
